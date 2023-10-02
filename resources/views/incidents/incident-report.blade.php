f27c7e18b58b76523889835569dbff21974e0bbd
Joel Aduma <caesaragen@gmail.com>
2023-10-02 19:23:50 +0300

    add changes

--- resources/views/incidents/incident-report.blade.php
+++ resources/views/incidents/incident-report.blade.php
@@ -17,11 +17,11 @@
                             <h6 class="text-blueGray-700 text-xl font-bold">
                                 My account
                             </h6>
-                            <button
+                            {{-- <button
                                 class="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
                                 type="button">
                                 Settings
-                            </button>
+                            </button> --}}
                         </div>
                     </div>
                     <div class="flex-auto px-4 lg:px-10 py-10 pt-0 bg-white">
